024de063fbf58e962e2ee73cc2481143c9fb24e5
jjunpro <alalstjr@naver.com>
2019-06-13 18:18:53 +0900

    project

--- Project-Boards/src/main/java/com/example/demo/repository/ProjectTaskRepository.java
+++ Project-Boards/src/main/java/com/example/demo/repository/ProjectTaskRepository.java
@@ -7,5 +7,5 @@ import com.example.demo.domain.ProjectTask;
 
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
-	ProjectTask getById(Long id);
+	ProjectTask getById(Long id); 
 }
